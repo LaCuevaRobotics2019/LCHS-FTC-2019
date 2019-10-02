3f3fee9ba7fc490a8a3c0d55d27cc3e35eede8fc
Jake Kasper <jbkasper03@gmail.com>
2019-10-02 10:50:15 -0600

    Created background of tele op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,7 +1,29 @@
 /*created on September 30, 2019 by Jake Kasper, Jake Gushard, Eli Hajagos, Edward Potapoe
 
  */
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="SuperNova", group="TeleOp")
 public class TeleOp extends OpMode{
+    //declare motors, servos, and sensors
 
+    public void init(){
+        //initiate the motors, servos, and sensors
+    }
+
+    public void active(){
+        //gamepad and throttle setup
+    }
+
+    double scaleInput(double dVal){
+        //scale input for throttle controls
+    }
 }
