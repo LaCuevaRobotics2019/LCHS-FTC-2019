4475fe5c7e4c847d4f3aee7b9840b8a6d25d313d
Jake Kasper <jbkasper03@gmail.com>
2019-10-01 22:19:17 -0600

    created the TeleOp class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,3 +1,7 @@
 /*created on September 30, 2019 by Jake Kasper in collaboration with
 
  */
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="SuperNova", group="TeleOp")
+public class TeleOp extends OpMode{
+
+}
