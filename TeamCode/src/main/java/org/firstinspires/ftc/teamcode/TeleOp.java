7e356f22291d0f53676a70c853bb58832b2e1046
Jake Kasper <jbkasper03@gmail.com>
2019-10-02 10:19:12 -0600

    added credits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,4 +1,4 @@
-/*created on September 30, 2019 by Jake Kasper in collaboration with
+/*created on September 30, 2019 by Jake Kasper in collaboration wit Jake Gushard, Eli Hajagos, Edward Potapoe
 
  */
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="SuperNova", group="TeleOp")
